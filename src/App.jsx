465098d3b06b331e4bccf3e4363fa2dfccfcd2f5
masy <masy@example.com>
2026-07-06 10:52:02 +0300

    Modified project's structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Head from "./header";
-import PageContent from "./content";
+import Head from "./Header";
+import PageContent from "./Content";
 import "./App.css";
 
 function App() {
